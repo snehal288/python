a21b05f9b1813f1c1b8d134e8a5bf5c5f58dc233
snehal288 <79372687+snehal288@users.noreply.github.com>
2021-06-24 12:14:48 +0530

    Add files via upload

---
+++ arithmatic.py
@@ -0,0 +1,17 @@
+#defination of addition function
+def add(no1,no2):
+    ans=no1+no2
+    return ans
+#defination of substraction function
+def sub(no1,no2):
+    ans=no1-no2
+    return ans
+def div(no1,no2):
+    ans=no1/no2
+    return ans
+def mult(no1,no2):
+    ans=no1*no2
+    return ans
+
+
+
